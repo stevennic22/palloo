edaf7491e331d4c244a5643916fe273455e41573
Steven N <schmoops01@gmail.com>
2016-01-05 09:10:15 -0500

    Removed unused code

--- palloo.php
+++ palloo.php
@@ -10,23 +10,6 @@ echo "<script type='text/javascript'>
 $(document).ready(function() {
   $('.test').hover(function(){ $(this).toggleClass('cn'); });
 });
-
-<!--
-var viewportwidth;
-var viewportheight;
-if (typeof window.innerWidth != 'undefined'){
-    viewportwidth = window.innerWidth,
-    viewportheight = window.innerHeight
-} else if (typeof document.documentElement != 'undefined'&& typeof document.documentElement.clientWidth !='undefined' && document.documentElement.clientWidth != 0){
-    viewportwidth = document.documentElement.clientWidth,
-    viewportheight = document.documentElement.clientHeight
-} else{
-    viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
-    viewportheight = document.getElementsByTagName('body')[0].clientHeight
-}
-//document.write('<p>Your viewport width is '+viewportwidth+'x'+viewportheight+'</p>');
-//-->
-}
 </script>";
 echo "<title>";
 
