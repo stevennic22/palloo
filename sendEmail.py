472ebb9b815b5811089ba0c548452a783f06d2a4
Steven N <schmoops01@gmail.com>
2016-06-29 00:58:44 -0400

    Fixed indentation
    Python hates when indentation is messed up, so I fixed it. Also made the
    log mildly more detailed.

--- sendEmail.py
+++ sendEmail.py
@@ -3,76 +3,79 @@
 import os, sys, getopt, smtplib, logging, datetime
 
 if(os.path.isdir("LOGS") == False):
-	os.makedirs("LOGS")
+  os.makedirs("LOGS")
 logFileName = os.path.normpath("LOGS\sendMail") + datetime.datetime.now().strftime("%d%m%y%H%M%S") + ".LOG"
 logging.basicConfig(format='%(levelname)s: %(message)s', filename=logFileName,level=logging.INFO)
 
 msgSetup = ["","",""]
 
 def errPrint():
-	print '-h, --help'
-	print '	prints this help command'
-	
+  print '-h, --help'
+  print ' prints this help command'
+  
 def arrayAddition(arg,i):
-	global msgSetup
-  logging.info("Adding " + arg + " to the list.")
-	msgSetup[i]=arg
-	return
+  global msgSetup
+  if i == 1:
+    logging.info("Adding '" + arg + "' (recipient) to the list.")
+  elif i == 0:
+    logging.info("Adding '" + arg + "' (message) to the list.")
+  elif i == 2:
+    logging.info("Adding '" + arg + "' (title) to the list.")
+  msgSetup[i]=arg
+  return
 
 def sendingMail():
-	global msgSetup
-	# Import the email modules we'll need
-	from email.mime.text import MIMEText
-	msg = MIMEText(msgSetup[0])
+  global msgSetup
+  # Import the email modules we'll need
+  from email.mime.text import MIMEText
+  msg = MIMEText(msgSetup[0])
 
-	# me == the sender's email address
-	# you == the recipient's email address
-	if msgSetup[2] == "":
+  # me == the sender's email address
+  # you == the recipient's email address
+  if msgSetup[2] == "":
     logging.warning("No title included. Inserting text 'No Subject' instead.")
-		msg['Subject'] = "No Subject"
-	else:
-		msg['Subject'] = msgSetup[2]
-	msg['From'] = "" #Put from email address
-	msg['To'] = msgSetup[1]
+    msg['Subject'] = "No Subject"
+  else:
+    msg['Subject'] = msgSetup[2]
+  msg['To'] = msgSetup[1]
 
-	# Send the message via our own SMTP server, but don't include the
-	# envelope header.
-	s = smtplib.SMTP('smtp.gmail.com', 587)
-	s.starttls()
-	s.login(msg['From'], "") #Put application specific password
-	s.sendmail(msg['From'], msg['To'], msg.as_string())
-	s.quit()
+  # Send the message via our own SMTP server, but don't include the
+  # envelope header.
+  s = smtplib.SMTP('smtp.gmail.com', 587)
+  s.starttls()
+  s.sendmail(msg['From'], msg['To'], msg.as_string())
+  s.quit()
   logging.info("Email sent.")
-	
+  
 def main(argv):
-	global msgSetup
+  global msgSetup
   logging.info("Starting to parse arguments")
-	try:
-		opts, args = getopt.getopt(argv,"hr:m:t:", ["help", "recipient=", "msg=", "title="])
-		if not opts:
-			# Return proper usage of script if in error
+  try:
+    opts, args = getopt.getopt(argv,"hr:m:t:", ["help", "recipient=", "msg=", "title="])
+    if not opts:
+      # Return proper usage of script if in error
       logging.warning("No arguments provided. Printing help message and closing.")
-			errPrint()
-	except getopt.GetoptError:
-		# Return proper usage of script if in error
+      errPrint()
+  except getopt.GetoptError:
+    # Return proper usage of script if in error
     logging.warning("No arguments provided. Printing help message and closing.")
-		errPrint()
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			# Return proper usage of script if in error
+    errPrint()
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      # Return proper usage of script if in error
       logging.info("Help message requested. Printing help message and closing.")
-			errPrint()
-			sys.exit(2)
-		elif opt in ("-r", "--recipient"):
-			arrayAddition(arg,1)
-		elif opt in ("-m", "--msg"):
-			arrayAddition(arg,0)
-		elif opt in ("-t", "--title"):
-			arrayAddition(arg,2)
+      errPrint()
+      sys.exit(2)
+    elif opt in ("-r", "--recipient"):
+      arrayAddition(arg,1)
+    elif opt in ("-m", "--msg"):
+      arrayAddition(arg,0)
+    elif opt in ("-t", "--title"):
+      arrayAddition(arg,2)
 
   logging.info("Attempting to send email.")
-	sendingMail()
-	
+  sendingMail()
+  
 if __name__ == '__main__':
     main(sys.argv[1:])
