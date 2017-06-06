4a3bb5daea3045e9c11e6a52c5aa33395180f4bf
Steven N <schmoops01@gmail.com>
2017-06-06 17:48:03 -0400

    Removed unnecessary global variable
    Moved information out of email sending file to make it easier to
    document (before moving to Gmail api).
    Added dicts instead of numbered array.
    Better error message and logging of error message

--- sendEmail.py
+++ sendEmail.py
@@ -15,45 +15,37 @@ formatter = logging.Formatter("[%(asctime)s] '%(levelname)s': {%(message)s}")
 handler.setFormatter(formatter)
 log.addHandler(handler)
 
-msgSetup = ["","",""]
-
 def errPrint():
-  print '-h, --help'
-  print ' prints this help command'
-  
-def arrayAddition(arg,i):
-  global msgSetup
-  if i == 1:
-    log.info("Adding '" + str(arg) + "' (recipient) to the list.")
-  elif i == 0:
-    log.info("Adding '" + str(arg) + "' (message) to the list.")
-  elif i == 2:
-    log.info("Adding '" + str(arg) + "' (title) to the list.")
-  msgSetup[i]=arg
-  return
+  helpMsg = '''-h, --help
+   prints this help command
+-r, --recipient
+   attaches new recipient to email
+-m, --msg
+   attaches email body content
+-t, --title
+   attaches new email title'''
+  print helpMsg
+  log.info(helpMsg)
 
-def sendingMail():
+def sendingMail(messageToSend):
   try:
-    global msgSetup
+    import json
+    with open('..' + os.sep + 'extensions.json', 'r') as eFile:
+      extensionInfo = json.load(eFile)
+
     # Import the email modules we'll need
     from email.mime.text import MIMEText
-    msg = MIMEText(msgSetup[0])
+    msg = MIMEText(messageToSend["msg"])
 
-    # me == the sender's email address
-    # you == the recipient's email address
-    if msgSetup[2] == "":
-      log.warning("No title included. Inserting text 'No Subject' instead.")
-      msg['Subject'] = "No Subject"
-    else:
-      msg['Subject'] = msgSetup[2]
-    msg['From'] = "" #Put from email address
-    msg['To'] = msgSetup[1]
+    msg['Subject'] = messageToSend["subject"]
+    msg['From'] = extensionInfo["palloo"]["email"]["address"]
+    msg['To'] = messageToSend["recipient"]
 
-    # Send the message via our own SMTP server, but don't include the
+    # Send the message via our Gmail server, but don't include the
     # envelope header.
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login(msg['From'], "") #Put application specific password
+    s.login(msg['From'], extensionInfo["palloo"]["email"]["key"])
     s.sendmail(msg['From'], msg['To'], msg.as_string())
     s.quit()
     log.info("Email sent.")
@@ -61,12 +53,17 @@ def sendingMail():
     log.error(e)
     sys.exit(1)
   except:
-    log.error("Unexpected error")
+    log.error("Unexpected error: ")
     log.error(sys.exc_info()[0])
+    log.error(sys.exc_info()[1])
     sys.exit(1)
   
 def main(argv):
-  global msgSetup
+  msgSetup = {
+    "recipient": "example@example.com",
+    "subject": "No Subject",
+    "msg": "Hello World"
+  }
   log.info("Starting to parse arguments")
   try:
     opts, args = getopt.getopt(argv,"hr:m:t:", ["help", "recipient=", "msg=", "title="])
@@ -74,6 +71,7 @@ def main(argv):
       # Return proper usage of script if in error
       log.warning("No arguments provided. Printing help message and closing.")
       errPrint()
+      exit()
   except getopt.GetoptError:
     # Return proper usage of script if in error
     log.warning("No arguments provided. Printing help message and closing.")
@@ -86,14 +84,15 @@ def main(argv):
       errPrint()
       sys.exit(2)
     elif opt in ("-r", "--recipient"):
-      arrayAddition(arg,1)
+      msgSetup["recipient"] = arg
     elif opt in ("-m", "--msg"):
-      arrayAddition(arg,0)
+      msgSetup["msg"] = arg
     elif opt in ("-t", "--title"):
-      arrayAddition(arg,2)
+      msgSetup["subject"] = arg
 
+  log.info(msgSetup)
   log.info("Attempting to send email.")
-  sendingMail()
+  sendingMail(msgSetup)
   
 if __name__ == '__main__':
     main(sys.argv[1:])
